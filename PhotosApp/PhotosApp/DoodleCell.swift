c8435f0dd7fb9e3c615dae241af840a1cb2a4e2c
hoonha choi <sosqsz2@naver.com>
2021-03-25 14:54:07 +0900

    UIMenuController를 통한 이미지 저장

--- PhotosApp/PhotosApp/DoodleCell.swift
+++ PhotosApp/PhotosApp/DoodleCell.swift
@@ -23,8 +23,27 @@ class DoodleCell: UICollectionViewCell {
         let gesture = UILongPressGestureRecognizer(target: self, action: #selector(touch))
         self.addGestureRecognizer(gesture)
     }
+
+    @objc func touch(_ sender: UILongPressGestureRecognizer) {
+        self.becomeFirstResponder()
+        let menuController = UIMenuController.shared
+        menuController.showMenu(from: self, rect: self.bounds)
+        let menuItem = UIMenuItem(title: "Save", action: #selector(save(_:)))
+        menuController.menuItems = [menuItem]
+    }
     
-    @objc func touch() {
-        
+    @objc func save(_ sender: UIMenuItem) {
+        guard let image = doodleImage.image else {
+            return
+        }
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return action == #selector(save(_:))
     }
 }
